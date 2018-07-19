9bcf3dd5ec080e479c2d28680707e5caa89ed663
Javed Gardezi <javed.gardezi@primexconnect.com>
2018-07-20 08:34:35 +1000

    Added new tables and laravel passport for oauth 2.0.

---
+++ application/database/migrations/2018_07_19_181307_alter_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->text('permissions')->nullable();
+            $table->timestamp('last_login')->nullable();
+            $table->timestamp('active')->nullable();
+            $table->string('activation_key',255)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('permissions');
+            $table->dropColumn('last_login');
+            $table->dropColumn('active');
+            $table->dropColumn('activation_key');
+        });
+    }
+}
